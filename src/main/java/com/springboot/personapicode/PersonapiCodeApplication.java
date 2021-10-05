c2c130d7e5324e9f4de910390f00df214aee8899
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2021-10-05 11:21:00 -0300

    Primeiro Commit

---
+++ src/main/java/com/springboot/personapicode/PersonapiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.personapicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/personapicode/PersonapiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.personapicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
